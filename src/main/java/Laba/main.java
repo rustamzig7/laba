6cbafdae8d4d3656770fc5a50079a33d6fb07167
Rustam <Rustamzig7@gmail.com>
2024-01-27 04:05:38 +0500

    Создание класса

---
+++ src/main/java/Laba/main.java
@@ -0,0 +1,18 @@
+package laba;
+
+import java.util.Random;
+
+
+public class RandomNumberGenerator {
+	   public int generateRandomNumber() {
+	        Random random = new Random();
+	        return random.nextInt(451) + 250;
+	    }
+	
+}
+
+
+
+public class main {
+
+}
