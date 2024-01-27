a37e1fc22afda6f49c18e25ff2b01c51ddeb41c3
Rustam <Rustamzig7@gmail.com>
2024-01-27 12:23:01 +0500

    Исправление Main

--- src/main/java/Laba/src/main/java/Laba/main.java
+++ src/main/java/Laba/src/main/java/Laba/main.java
@@ -1,34 +1,23 @@
 package laba;
-
 import java.util.Random;
 
-
-public class RandomNumberGenerator {
-	   public int generateRandomNumber() {
-	        Random random = new Random();
-	        return random.nextInt(451) + 250;
-	    }
-	
+class RandomNumberGenerator {
+    public int generateRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(451) + 250;
+    }
 }
 
-
-
-public class main {
-    public static void main(String[] args) {
-        ArrayCreator arrayCreator = new ArrayCreator();
-        int[] randomArray = arrayCreator.createRandomArray();
-        System.out.println("Получение радомные числа:");
-        for (int i : randomArray) {
-            System.out.print(i + " ");
+class ArrayCreator {
+    public int[] createRandomArray() {
+        int[] array = new int[12];
+        RandomNumberGenerator rng = new RandomNumberGenerator();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rng.generateRandomNumber();
         }
-        System.out.println();
-
-        BubbleSort bubbleSort = new BubbleSort();
-        bubbleSort.sortArray(randomArray);
-        System.out.println("Сортировка массива:");
-        for (int i : randomArray) {
-            System.out.print(i + " ");
-        }
+        return array;
+    }
+}
 
 class BubbleSort {
     public void sortArray(int[] array) {
@@ -46,17 +35,21 @@ class BubbleSort {
 }
 
 
-
-class ArrayCreator {
-    public int[] createRandomArray() {
-        int[] array = new int[12];
-        RandomNumberGenerator rng = new RandomNumberGenerator();
-        for (int i = 0; i < array.length; i++) {
-            array[i] = rng.generateRandomNumber();
+public class Main {
+    public static void main(String[] args) {
+        ArrayCreator arrayCreator = new ArrayCreator();
+        int[] randomArray = arrayCreator.createRandomArray();
+        System.out.println("Получение радомные числа:");
+        for (int i : randomArray) {
+            System.out.print(i + " ");
         }
-        return array;
+        System.out.println();
 
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sortArray(randomArray);
+        System.out.println("Сортировка массива:");
+        for (int i : randomArray) {
+            System.out.print(i + " ");
+        }
     }
 }
-
-
